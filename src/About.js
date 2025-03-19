c60847857be8bb44b9d1329ce407e826b9bdfeb0
Bhavana <thakurbhavana2004@gmail.com>
2025-03-19 18:49:24 +0530

    hii

---
+++ src/About.js
@@ -0,0 +1,14 @@
+function Myabout(){
+return <h1>this is first page</h1>
+}
+export default Myabout
+
+export function Contactus()
+{
+  <h1> this is contact session</h1>
+}
+function Mypage()
+{
+return
+<div>this is heading page</div>
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import  ReactDOM from  'react-dom/client';
+import "./style.css";
+import Abc, {Contactus, Mypage} from './About';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(
+   <React.StrictMode>
+
+   </React.StrictMode>
+ );
+
+
+
+
+
